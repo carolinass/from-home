c2fe5c11cf36abc5e7cff7b4910343be31160a42
carolinass <carolzinhacabral@gmail.com>
2020-10-24 23:11:46 -0400

    Styling login page

--- screens/Auth/LoginScreen.tsx
+++ screens/Auth/LoginScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import * as firebase from 'firebase';
 import { StackScreenProps } from '@react-navigation/stack';
-import { BaseLayout } from '../../components/layout';
-import { Button, Form, Input, Item, Label, Text, Toast } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Button, Form, Input, Item, Label, Text, Toast, View } from 'native-base';
 import { useUser } from '../../hooks/useUser';
+import { Image, ImageBackground } from 'react-native';
 
 const LoginScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
   const [email, setEmail] = useState('');
@@ -25,28 +26,64 @@ const LoginScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
   }, [email, password, navigation])
 
   return (
-    <BaseLayout title="Login">
+    <ImageBackground
+      source={{ uri: 'https://blog.vindi.com.br/wp-content/uploads/2020/03/home-office-scaled.jpg' }}
+      style={styles.imageBackground}
+    >
+      <View style={styles.overlay}>
+      <Image
+        style={styles.logo}
+        source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/nonow-e7237.appspot.com/o/logo%20(4).png?alt=media&token=e9c927f9-cddb-4d31-8253-5593c513febf' }}
+      />
       <Form>
-        <Item floatingLabel style={{ margin: 10 }} >
+        <Item floatingLabel style={styles.formInput}>
           <Label>E-Mail</Label>
           <Input keyboardType='email-address' value={email} onChangeText={setEmail} />
         </Item>
 
-        <Item floatingLabel style={{ margin: 10 }}>
+        <Item floatingLabel style={styles.formInput}>
           <Label>Password</Label>
           <Input secureTextEntry={true} value={password} onChangeText={setPassword} />
         </Item>
 
-        <Button primary block style={{ margin: 10 }} onPress={onLogin}>
+        <Button dark block style={styles.loginBtn} onPress={onLogin}>
           <Text>Login</Text>
         </Button>
       </Form>
 
       <Button transparent onPress={() => navigation.navigate('Signup')}>
-        <Text>Don't have an account yet?</Text>
+        <Text style={{ color: 'grey' }}>Don't have an account yet?</Text>
       </Button>
-    </BaseLayout>
+      </View>
+    </ImageBackground>
   )
 }
+
+const styles = StyleSheet.create({
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: "center"
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor:'rgba(255,255,255,0.8)'
+  },
+  logo: {
+    alignSelf: "center",
+    width: 300 ,
+    height: 35,
+    marginBottom: 30
+  },
+  loginBtn: {
+    margin: 10,
+    marginTop: 25
+  },
+  formInput: {
+    margin: 10,
+    borderColor: 'black'
+  }
+});
 
 export default LoginScreen
