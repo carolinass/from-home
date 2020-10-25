4f138b018b3263ca94c5c1b5b8e9785d29c307a0
Jan Houben <jhouben@qmo.io>
2020-10-24 20:28:47 -0400

    Ios timepicker fix

--- components/TimePicker.tsx
+++ components/TimePicker.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { format } from 'date-fns'
-import { Text, View } from 'native-base'
+import { Button, Text, View } from 'native-base'
+import { Platform } from 'react-native'
 
 type TimePickerProps = {
   value: Date | undefined
@@ -15,18 +16,27 @@ const TimePicker: React.FC<TimePickerProps> = ({ value = new Date(), onChange, .
   return (
     <View style={{ margin: 10 }}>
       <TouchableOpacity onPress={() => setIsVisible(true)}>
-        <Text>{format(value, 'p')}</Text>
+        <Text>{!isVisible && format(value, 'p')}</Text>
       </TouchableOpacity>
       {isVisible && (
         <DateTimePicker
           {...props}
+          style={{ minWidth: 200 }}
           mode="time"
           value={value}
           onChange={(e, date) => {
-            setIsVisible(false)
+            if (Platform.OS === 'android') {
+              setIsVisible(false)
+            }
             onChange(date)
           }}
         />
+      )}
+
+      {isVisible && Platform.OS !== 'android' && (
+        <Button light onPress={() => setIsVisible(false)}>
+          <Text>Apply</Text>
+        </Button>
       )}
     </View>
   )
