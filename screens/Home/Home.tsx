d28a7ee8e9fc2821167e7bbecdf93a7bca47bd8b
carolinass <carolzinhacabral@gmail.com>
2020-10-24 21:23:35 -0400

    Adding room status

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -4,6 +4,8 @@ import * as firebase from 'firebase'
 import { useUser } from '../../hooks/useUser';
 import { BaseLayout } from '../../components/layout';
 import { StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { isBefore, isAfter } from 'date-fns'
 
 interface Home {
   id: string
@@ -18,6 +20,7 @@ const Home = ({ navigation }) => {
   const [home, setHome] = useState<Home>()
   const [roomates, setRoomates] = useState<any[]>([])
   const [rooms, setRooms] = useState<any[]>([])
+  const [events, setEvents] = useState<any[]>([])
 
   const goToCreateHome = () => {
     navigation.navigate('Home', { screen: 'Create Home' });
@@ -32,11 +35,11 @@ const Home = ({ navigation }) => {
   }
 
   const goToInviteFriend = () => {
-    navigation.navigate('Invite Friend', { screen: 'Create Home' });
+    navigation.navigate('Home', { screen: 'Invite Friend' });
   }
 
-  const newEvent = () => {
-    console.log('new event')
+  const goToNewEvent = () => {
+    navigation.navigate('Home', { screen: 'Schedule Event' });
   }
 
   const getRoomates = async (homeId: string) => {
@@ -69,6 +72,32 @@ const Home = ({ navigation }) => {
     );
   }
 
+  const getTodayEvents = (homeId: string) => {
+    const db = firebase.firestore();
+    db.collection('events')
+      .where('startDate', '<=', new Date())
+      .where('homeId', '==', homeId)
+      .onSnapshot((querySnapshot) => {
+        const events: React.SetStateAction<any[]> = [];
+        querySnapshot.forEach((doc) => {
+          events.push({ id: doc.id, ...doc.data() });
+        });
+        setEvents(events)
+      }
+    );
+  }
+
+  const isRoomAvailable = (room: any) => {
+    for (const event of events) {
+      if (event.room === room.id) {
+        if (isBefore(event.startDate.toDate(), Date.now()) && isAfter(event.endDate.toDate(), Date.now())) {
+          return event
+        }
+      }
+    }
+    return null
+  }
+
   useEffect(() => {
     if (!user || !user.homeId) return
     const homeId = user.homeId
@@ -78,13 +107,28 @@ const Home = ({ navigation }) => {
       setHome({ id: doc.id, ...doc.data() })
       getRoomates(doc.id)
       getRooms(doc.id)
+      getTodayEvents(doc.id)
     })
   }, [user])
+
+  const roomItem = (room: any, i: number) => {
+    const roomEvent = isRoomAvailable(room)
+    const roomText = roomEvent ? `${room.name} - ${roomEvent['title']}` : room.name
+    return (
+      <ListItem key={i}>
+        {roomEvent
+          ? <MaterialIcons name="event-busy" size={24} color="#F04747" style={styles.roomStatus} />
+          : <MaterialIcons name="event-available" size={24} color="green" style={styles.roomStatus} />
+        }
+        <Text>{roomText}</Text>
+      </ListItem>
+    )
+  }
 
   return (
     user && user.homeId && home ?
       <BaseLayout title={home.name}>
-        <Button block onPress={newEvent} style={styles.extraMargin}>
+        <Button block onPress={goToNewEvent} style={styles.extraMargin}>
           <Text>New Event</Text>
         </Button>
         <List style={styles.extraMargin}>
@@ -105,9 +149,7 @@ const Home = ({ navigation }) => {
             <H3>ROOMS</H3>
           </ListItem>
           {rooms.map((room, i) =>
-            <ListItem key={i}>
-              <Text>{room.name}</Text>
-            </ListItem>
+            roomItem(room, i)
           )}
         </List>
         <Button transparent onPress={goToCreateRoom}>
@@ -130,6 +172,9 @@ const styles = StyleSheet.create({
   extraMargin: {
     marginTop: 15,
     marginBottom: 15
+  },
+  roomStatus: {
+    marginRight: 10
   }
 });
 
